69a044bbe792213acfaf45901217085680262b0d
Ideborah-tech <dbingabire05@gmail.com>
2025-04-23 16:14:28 +0200

    codes refused to work

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -26,11 +26,15 @@ proper order even if all the requests haven't finished.
    * @param  {Object} data - The raw data describing the planet.
    */
   function createPlanetThumb(data) {
-    var pT = document.createElement("planet-thumb");
-    for (var d in data) {
-      pT[d] = data[d];
-    }
-    home.appendChild(pT);
+    return new Promise(function(resolve) {
+      var pT = document.createElement('planet-thumb');
+      for (var d in data) {
+        pT[d] = data[d];
+      }
+      home.appendChild(pT);
+      console.log('rendered: ' + data.pl_name);
+      resolve();
+    });
   }
 
   /**
@@ -50,16 +54,49 @@ proper order even if all the requests haven't finished.
    * @return {Promise}    - A promise that passes the parsed JSON response.
    */
   function getJSON(url) {
-    return get(url).then(function (response) {
-      return response.json();
+    console.log('sent: ' + url);
+    return get(url).then(function(response) {
+      // For testing purposes, I'm making sure that the urls don't return in order
+      if (url === 'data/planets/Kepler-62f.json') {
+        return new Promise(function(resolve) {
+          setTimeout(function() {
+            console.log('received: ' + url);
+            resolve(response.json());
+          }, 500);
+        });
+      } else {
+        console.log('received: ' + url);
+        return response.json();
+      }
     });
   }
+  
 
-  window.addEventListener("WebComponentsReady", function () {
+  window.addEventListener('WebComponentsReady', function() {
     home = document.querySelector('section[data-route="home"]');
-    /*
-    Your code goes here!
-     */
-    // getJSON('../data/earth-like-results.json')
-  });
-})(document);
+  
+    getJSON('../data/earth-like-results.json')
+    .then(function(response) {
+      addSearchHeader(response.query);
+      return response;
+    })
+    .then(function(response) {
+      var sequence = Promise.resolve();
+  
+      // .map executes all of the network requests immediately.
+      var arrayOfExecutingPromises = response.results.map(function(result) {
+        return getJSON(result);
+      });
+  
+      arrayOfExecutingPromises.forEach(function(request) {
+        // Loop through the pending requests that were returned by .map (and are in order) and
+        // turn them into a sequence.
+        // request is a getJSON() that's currently executing.
+        sequence = sequence.then(function() {
+          // Remember that createPlanetThumb is a Promise, so it must resolve before Promises
+          // later in the sequence can execute.
+          return request.then(createPlanetThumb);
+        });
+      });
+    });
+  })(document);
